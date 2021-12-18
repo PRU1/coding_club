f0c9dcbe6b4829e21b97dd6974a8bd9520afb392
PRU1 <52003426+PRU1@users.noreply.github.com>
2021-12-17 22:33:35 -0500

    Create slide_5_1.java

---
+++ slide_5_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+    
+    for (int i = 0; i <= 10000; i+=2){
+        System.out.print(i + " ");
+    }
+	}
+}
